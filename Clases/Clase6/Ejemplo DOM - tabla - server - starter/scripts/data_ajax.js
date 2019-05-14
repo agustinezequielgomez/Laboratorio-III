51a540fe1fbce8c62d70739ce22ba0db53a96c75
agustinezequielgomez <agusgomez99@gmail.com>
2019-05-14 17:26:57 -0300

    Cambios utiles dentro de todo

--- Clases/Clase6/Ejemplo DOM - tabla - server - starter/scripts/data_ajax.js
+++ Clases/Clase6/Ejemplo DOM - tabla - server - starter/scripts/data_ajax.js
@@ -1,9 +1,10 @@
 //Enviar peticion GET a http://localhost:3000/traer
 //pasar parametro "collection"  con valor "personas"  
 //La respuesta sera un array con las personas
+
 var xhr;
-function traerPersonas() 
-{
+
+function traerPersonas() {
     xhr = new XMLHttpRequest();
     xhr.onreadystatechange = procesarPersonas;
     var cadena = "http://localhost:3000/traer?collection=personas";
@@ -11,8 +12,7 @@ function traerPersonas()
     xhr.send();
 }
 
-function traer()
-{
+function traer() {
     xhr = new XMLHttpRequest();
     xhr.onreadystatechange = procesarPersonas;
     var cadena = "http://localhost:3000/traerpersona?collection=personas&indice=20";
@@ -20,26 +20,43 @@ function traer()
     xhr.send();
 }
 
-function enviarPersonas()
-{
-    
+function enviar() {
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = enviarPersonas;
+    var cadena = "http://localhost:3000/enviar";
+    var body = { "collection": "personas", ReArmarTabla() };
+    xhr.open('POST', cadena, true);
+    xhr.send(JSON.stringify(body));
 }
 
-function procesarPersonas() 
-{
-    var josele = document.getElementById('spinner')
+function enviarPersonas() {
+    var spinner = document.getElementById('spinner')
     if (xhr.readyState == 4) {
         if (xhr.status == 200) {
             arrayPersonas = JSON.parse(xhr.responseText);
             construirTabla(arrayPersonas);
-            josele.style.display = "none";
-        }
-        else {
+            spinner.style.display = "none";
+        } else {
             alert("Error: " + xhr.status + " - " + xhr.statusText);
             console.log(xhr.responseText);
         }
+    } else {
+        spinner.style.display = "block";
     }
-    else {
-        josele.style.display= "block";
+}
+
+function procesarPersonas() {
+    var spinner = document.getElementById('spinner')
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            arrayPersonas = JSON.parse(xhr.responseText);
+            construirTabla(arrayPersonas);
+            spinner.style.display = "none";
+        } else {
+            alert("Error: " + xhr.status + " - " + xhr.statusText);
+            console.log(xhr.responseText);
+        }
+    } else {
+        spinner.style.display = "block";
     }
 }
--- Clases/Clase6/Ejemplo DOM - tabla - server - starter/scripts/index.js
+++ Clases/Clase6/Ejemplo DOM - tabla - server - starter/scripts/index.js
@@ -1,60 +1,50 @@
-window.addEventListener('load',function()
-{
+window.addEventListener('load', function() {
     boton = document.getElementById('armarTabla');
-    boton.addEventListener('click',traerPersonas);
+    boton.addEventListener('click', traerPersonas);
 });
 
 
-function construirTabla(arrayDatos)
-{
+function construirTabla(arrayDatos) {
     body = document.getElementsByTagName("body");
     tabla = document.createElement("table");
-    tabla.setAttribute('id','tablaDatos');
+    tabla.setAttribute('id', 'tablaDatos');
     var cabecera = document.createElement("tr");
-    cabecera.setAttribute('id',"tableHeaeder");
-    for(atributo in arrayDatos[0])
-    {
+    cabecera.setAttribute('id', "tableHeaeder");
+    for (atributo in arrayDatos[0]) {
         header = document.createElement("th");
         header.appendChild(document.createTextNode(atributo));
         cabecera.appendChild(header);
     }
     tabla.appendChild(cabecera);
 
-    for(dato of arrayDatos)
-    {
+    for (dato of arrayDatos) {
         var fila = document.createElement("tr");
-        fila.setAttribute('class','personas');
-        var atributosExtra = DetectarAtributos(dato,cabecera.childNodes);
-        if(atributosExtra!=[])
-        {
-            for(atributoAgregar of atributosExtra)
-            {
+        fila.setAttribute('class', 'personas');
+        var atributosExtra = DetectarAtributos(dato, cabecera.childNodes);
+        if (atributosExtra != []) {
+            for (atributoAgregar of atributosExtra) {
                 th = document.createElement('th');
                 th.appendChild(document.createTextNode(atributoAgregar));
                 cabecera.appendChild(th);
             }
         }
-        for(atributo in dato)
-        {
+        for (atributo in dato) {
             td = document.createElement("td");
             td.appendChild(document.createTextNode(dato[atributo]));
             fila.appendChild(td);
         }
-        fila.addEventListener('click',seleccionarRegistro);
+        fila.addEventListener('click', seleccionarRegistro);
         tabla.appendChild(fila);
     }
     document.body.appendChild(tabla);
 }
 
-function seleccionarRegistro()
-{
+function seleccionarRegistro() {
     var body = document.getElementsByTagName('body');
-    for(hijos of body[0].children)
-    {
+    for (hijos of body[0].children) {
         console.log(hijos.nodeName);
-        if(hijos.nodeName == "FORM")
-        {
-            return -1;   
+        if (hijos.nodeName == "FORM") {
+            return -1;
         }
     }
     var formulario = document.createElement('form');
@@ -63,18 +53,16 @@ function seleccionarRegistro()
     legend.appendChild(document.createTextNode('Datos personales'));
     groupBox.appendChild(legend);
     var cabecera = document.getElementById("tableHeaeder");
-    for(var i = 0; i< cabecera.childElementCount ; i++)
-    {
+    for (var i = 0; i < cabecera.childElementCount; i++) {
         var label = document.createElement('label');
         var texto = document.createTextNode(cabecera.children[i].innerText);
         label.appendChild(texto);
         groupBox.appendChild(label);
         var input = document.createElement('input');
-        input.setAttribute('value',(this.children[i].innerText));
-        input.setAttribute('class','formInputs');
-        if(cabecera.children[i].innerText == "id")
-        {
-            input.setAttribute('disabled','true');
+        input.setAttribute('value', (this.children[i].innerText));
+        input.setAttribute('class', 'formInputs');
+        if (cabecera.children[i].innerText == "id") {
+            input.setAttribute('disabled', 'true');
         }
         groupBox.appendChild(input);
         groupBox.appendChild(document.createElement('br'));
@@ -82,86 +70,71 @@ function seleccionarRegistro()
     formulario.appendChild(groupBox);
     var boton = document.createElement('button');
     boton.appendChild(document.createTextNode('Actualizar'));
-    boton.setAttribute('type','button');
-    boton.addEventListener('click',actualizarRegistro);
+    boton.setAttribute('type', 'button');
+    boton.addEventListener('click', actualizarRegistro);
     groupBox.appendChild(boton);
     document.body.appendChild(formulario);
 }
 
-function actualizarRegistro()
-{
+function actualizarRegistro() {
     console.log(this);
     var inputs = document.getElementsByClassName('formInputs');
     var fila = buscarRegistro(inputs[0].value);
-    for(var i = 1; i<fila.childElementCount; i++)
-    {
+    for (var i = 1; i < fila.childElementCount; i++) {
         fila.children[i].innerText = inputs[i].value;
     }
     document.body.removeChild(this.parentElement.parentElement);
     ReArmarTabla();
 }
 
-function buscarRegistro(id)
-{
+function buscarRegistro(id) {
     var tabla = document.getElementById('tablaDatos');
-    for(filas of tabla.children)
-    {
-        for(celdas of filas.children)
-        {
-            if(id == celdas.innerText)
-            {
+    for (filas of tabla.children) {
+        for (celdas of filas.children) {
+            if (id == celdas.innerText) {
                 return filas;
             }
         }
     }
 }
 
-function DetectarAtributos(objeto,atributos)
-{
+function DetectarAtributos(objeto, atributos) {
     var keys = Object.keys(objeto);
     var retorno = [];
     var flag;
-    for(valor of keys)
-    {
+    for (valor of keys) {
         flag = false;
-        for(val of atributos)
-        {
-            if(val.innerText == valor)
-            {
+        for (val of atributos) {
+            if (val.innerText == valor) {
                 flag = true;
                 break;
             }
         }
-        if(flag == false)
-        {
+        if (flag == false) {
             retorno.push(valor);
         }
     }
     return retorno;
 }
 
-function ReArmarTabla()
-{
+function ReArmarTabla() {
     var header = document.getElementById('tableHeaeder');
     var personas = document.getElementsByClassName('personas');
     var columnas = [];
     var retorno = "";
-    for(cols of header.children)
-    {
+    for (cols of header.children) {
         columnas.push(cols.innerText);
     }
-    for(persona of personas)
-    {
+    for (persona of personas) {
         retorno += "{";
-        for(var i = 0 ; i < columnas.length ; i++)
-        {
-            retorno += columnas[i]+":"+persona.children[i].innerText;
-            if(i+1<columnas.length)
-            {
+        for (var i = 0; i < columnas.length; i++) {
+            retorno += columnas[i] + ":" + persona.children[i].innerText;
+            if (i + 1 < columnas.length) {
                 retorno += ",";
             }
         }
         retorno += "},";
     }
     console.log(JSON.stringify(retorno));
+    return retorno;
 }
