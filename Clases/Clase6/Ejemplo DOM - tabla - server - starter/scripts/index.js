f669e028458499068ec8d407a8cf75309dc4da3a
agustinezequielgomez <agusgomez99@gmail.com>
2019-05-13 21:32:35 -0300

    Revert "No se que hice en realidad"
    This reverts commit b590ff1190d1d4a899e06a6ad1f64d23767b37f2.
    
    asadsasdsdfgdfgndfjgndfjgdfjgndbfbgir
    sdfsdfsdf
    sdfsdfsdfds

--- Clases/Clase6/Ejemplo DOM - tabla - server - starter/scripts/index.js
+++ Clases/Clase6/Ejemplo DOM - tabla - server - starter/scripts/index.js
@@ -1,61 +1,71 @@
-window.addEventListener('load', function() {
+window.addEventListener('load',function()
+{
     boton = document.getElementById('armarTabla');
-    boton.addEventListener('click', traerPersonas);
+    boton.addEventListener('click',traerPersonas);
 });
 
 
-function construirTabla(arrayDatos) {
+function construirTabla(arrayDatos)
+{
     body = document.getElementsByTagName("body");
     tabla = document.createElement("table");
-    tabla.setAttribute('id', 'tablaDatos');
+    tabla.setAttribute('id','tablaDatos');
     var cabecera = document.createElement("tr");
-    cabecera.setAttribute('id', "tableHeaeder");
-    for (atributo in arrayDatos[0]) {
+    cabecera.setAttribute('id',"tableHeaeder");
+    for(atributo in arrayDatos[0])
+    {
         header = document.createElement("th");
         header.appendChild(document.createTextNode(atributo));
         cabecera.appendChild(header);
     }
     tabla.appendChild(cabecera);
 
-    for (dato of arrayDatos) {
+    for(dato of arrayDatos)
+    {
         var fila = document.createElement("tr");
-        fila.setAttribute('class', 'personas');
-        var atributosExtra = DetectarAtributos(dato, cabecera.childNodes);
-        if (atributosExtra != []) {
-            for (atributoAgregar of atributosExtra) {
+        fila.setAttribute('class','personas');
+        var atributosExtra = DetectarAtributos(dato,cabecera.childNodes);
+        if(atributosExtra!=[])
+        {
+            for(atributoAgregar of atributosExtra)
+            {
                 th = document.createElement('th');
                 th.appendChild(document.createTextNode(atributoAgregar));
                 cabecera.appendChild(th);
             }
         }
-        for (atributo in dato) {
+        for(atributo in dato)
+        {
             td = document.createElement("td");
             td.appendChild(document.createTextNode(dato[atributo]));
             fila.appendChild(td);
         }
-        fila.addEventListener('click', seleccionarRegistro);
+        fila.addEventListener('click',seleccionarRegistro);
         tabla.appendChild(fila);
     }
     document.body.appendChild(tabla);
 }
 
-function seleccionarRegistro() {
+function seleccionarRegistro()
+{
     var formulario = document.createElement('form');
     var groupBox = document.createElement('fieldset');
     var legend = document.createElement('legend');
     legend.appendChild(document.createTextNode('Datos personales'));
     groupBox.appendChild(legend);
     var cabecera = document.getElementById("tableHeaeder");
-    for (var i = 0; i < cabecera.childElementCount; i++) {
+    for(var i = 0; i< cabecera.childElementCount ; i++)
+    {
         var label = document.createElement('label');
         var texto = document.createTextNode(cabecera.children[i].innerText);
         label.appendChild(texto);
         groupBox.appendChild(label);
         var input = document.createElement('input');
-        input.setAttribute('value', (this.children[i].innerText));
-        input.setAttribute('class', 'formInputs');
-        if (cabecera.children[i].innerText == 'id') {
-            input.setAttribute('disabled', 'true');
+        input.setAttribute('value',(this.children[i].innerText));
+        input.setAttribute('class','formInputs');
+        if(i == 0)
+        {
+            input.setAttribute('disabled','true');
         }
         groupBox.appendChild(input);
         groupBox.appendChild(document.createElement('br'));
@@ -63,45 +73,57 @@ function seleccionarRegistro() {
     formulario.appendChild(groupBox);
     var boton = document.createElement('button');
     boton.appendChild(document.createTextNode('Actualizar'));
-    boton.setAttribute('type', 'button');
-    boton.addEventListener('click', actualizarRegistro);
+    boton.setAttribute('type','button');
+    boton.addEventListener('click',actualizarRegistro);
     groupBox.appendChild(boton);
     document.body.appendChild(formulario);
 }
 
-function actualizarRegistro() {
+function actualizarRegistro()
+{
+    console.log(this);
     var inputs = document.getElementsByClassName('formInputs');
     var fila = buscarRegistro(inputs[0].value);
-    for (var i = 1; i < fila.childElementCount; i++) {
+    for(var i = 1; i<fila.childElementCount; i++)
+    {
         fila.children[i].innerText = inputs[i].value;
     }
     document.body.removeChild(this.parentElement.parentElement);
 }
 
-function buscarRegistro(id) {
+function buscarRegistro(id)
+{
     var tabla = document.getElementById('tablaDatos');
-    for (filas of tabla.children) {
-        for (celdas of filas.children) {
-            if (id == celdas.innerText) {
+    for(filas of tabla.children)
+    {
+        for(celdas of filas.children)
+        {
+            if(id == celdas.innerText)
+            {
                 return filas;
             }
         }
     }
 }
 
-function DetectarAtributos(objeto, atributos) {
+function DetectarAtributos(objeto,atributos)
+{
     var keys = Object.keys(objeto);
     var retorno = [];
     var flag;
-    for (valor of keys) {
+    for(valor of keys)
+    {
         flag = false;
-        for (val of atributos) {
-            if (val.innerText == valor) {
+        for(val of atributos)
+        {
+            if(val.innerText == valor)
+            {
                 flag = true;
                 break;
             }
         }
-        if (flag == false) {
+        if(flag == false)
+        {
             retorno.push(valor);
         }
     }
